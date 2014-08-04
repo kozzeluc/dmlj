874bc56f65de8d6bf1f5fdea5253013c03f719b2
Luc Hermans <kozzeluc@gmail.com>
2014-08-04 09:37:45 +0200

    Complete Driver Information page.

--- org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.dictionary.tools/src/org/lh/dmlj/schema/editor/dictionary/tools/Plugin.java
+++ org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.dictionary.tools/src/org/lh/dmlj/schema/editor/dictionary/tools/Plugin.java
@@ -1,18 +1,33 @@
+/**
+ * Copyright (C) 2014  Luc Hermans
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact information: kozzeluc@gmail.com.
+ */
 package org.lh.dmlj.schema.editor.dictionary.tools;
 
 import java.sql.Driver;
 import java.sql.DriverManager;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 
-/**
- * The activator class controls the plug-in life cycle
- */
 public class Plugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.lh.dmlj.schema.editor.dictionary.tools"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.lh.dmlj.schema.editor.dictionary.tools";
 
 	// The shared instance
 	private static Plugin plugin;
@@ -21,6 +36,11 @@ public class Plugin extends AbstractUIPlugin {
 	private static final String DRIVER_NOT_INSTALLED = "NOT INSTALLED";
 	
 	private String driverVersion = DRIVER_NOT_INSTALLED;
+	
+	private String driverBundleId ="N/A";
+	private String driverBundleVersion ="N/A";
+	private String driverBundleName ="N/A";
+	private String driverBundleVendor ="N/A";
 
 	static {
 		try {
@@ -29,21 +49,29 @@ public class Plugin extends AbstractUIPlugin {
 		}
 	}
 
-	/**
-	 * The constructor
-	 */
 	public Plugin() {
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static Plugin getDefault() {
 		return plugin;
 	}
 
+	public String getDriverBundleId() {
+		return driverBundleId;
+	}
+	
+	public String getDriverBundleName() {
+		return driverBundleName;
+	}
+	
+	public String getDriverBundleVendor() {
+		return driverBundleVendor;
+	}
+	
+	public String getDriverBundleVersion() {
+		return driverBundleVersion;
+	}
+	
 	public String getDriverVersion() {
 		return driverVersion;
 	}
@@ -52,24 +80,25 @@ public class Plugin extends AbstractUIPlugin {
 		return !driverVersion.equals(DRIVER_NOT_INSTALLED);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		getDriverInformation();
+	}
+	
+	private void getDriverInformation() {
 		try {
 			Driver driver = DriverManager.getDriver("jdbc:idms://xyz/APPLDICT");
 			driverVersion = driver.getMajorVersion() + "." + driver.getMinorVersion();
+			Bundle bundle = FrameworkUtil.getBundle(driver.getClass());
+			driverBundleId = bundle.getSymbolicName();
+			driverBundleVersion = bundle.getVersion().toString();
+			driverBundleName = bundle.getHeaders().get("Bundle-Name");
+			driverBundleVendor = bundle.getHeaders().get("Bundle-Vendor");
 		} catch (Throwable t) {
 		}
 	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
--- org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.dictionary.tools/src/org/lh/dmlj/schema/editor/dictionary/tools/preference/DriverInformationPage.java
+++ org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.dictionary.tools/src/org/lh/dmlj/schema/editor/dictionary/tools/preference/DriverInformationPage.java
@@ -23,12 +23,22 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.lh.dmlj.schema.editor.dictionary.tools.Plugin;
+import org.eclipse.swt.widgets.Group;
 
 public class DriverInformationPage extends PreferencePage implements IWorkbenchPreferencePage {
+	
+	private Text textVersion;
+	private Text textId;
+	private Text textBundleVersion;
+	private Text textBundleName;
+	private Text textBundleVendor;
 
 	public DriverInformationPage() {
 		super();
-		setDescription("Driver information:");
 	}
 
 	@Override
@@ -38,12 +48,65 @@ public class DriverInformationPage extends PreferencePage implements IWorkbenchP
 	@Override
 	protected Control createContents(Composite parent) {
 
-		final Composite container = new Composite(parent, SWT.NONE);		
-		GridLayout layout = new GridLayout(3, false);
-		container.setLayout(layout);
+		final Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new GridLayout(2, false));
+		
+		Group grpDriverInformation = new Group(container, SWT.NONE);
+		grpDriverInformation.setLayout(new GridLayout(2, false));
+		grpDriverInformation.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		grpDriverInformation.setText("CA IDMS JDBC Driver information");
+		
+		Label lblVersion = new Label(grpDriverInformation, SWT.NONE);
+		lblVersion.setText("Version:");
+		
+		textVersion = new Text(grpDriverInformation, SWT.BORDER | SWT.READ_ONLY);
+		textVersion.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Group grpProviderPlugin = new Group(container, SWT.NONE);
+		grpProviderPlugin.setText("Provider plug-in");
+		grpProviderPlugin.setLayout(new GridLayout(2, false));
+		GridData gd_grpProviderPlugin = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_grpProviderPlugin.verticalIndent = 10;
+		grpProviderPlugin.setLayoutData(gd_grpProviderPlugin);
+		
+		Label lblNewLabel = new Label(grpProviderPlugin, SWT.NONE);
+		lblNewLabel.setText("ID:");
+		
+		textId = new Text(grpProviderPlugin, SWT.BORDER | SWT.READ_ONLY);
+		textId.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblNewLabel_1 = new Label(grpProviderPlugin, SWT.NONE);
+		lblNewLabel_1.setText("Version:");
+		
+		textBundleVersion = new Text(grpProviderPlugin, SWT.BORDER | SWT.READ_ONLY);
+		textBundleVersion.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblNewLabel_2 = new Label(grpProviderPlugin, SWT.NONE);
+		lblNewLabel_2.setText("Name:");
+		
+		textBundleName = new Text(grpProviderPlugin, SWT.BORDER | SWT.READ_ONLY);
+		textBundleName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblNewLabel_3 = new Label(grpProviderPlugin, SWT.NONE);
+		lblNewLabel_3.setText("Vendor:");
+		
+		textBundleVendor = new Text(grpProviderPlugin, SWT.BORDER | SWT.READ_ONLY);
+		textBundleVendor.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		initialize();
 		
 		return container;
 		
 	}
 
+	private void initialize() {
+		
+		textVersion.setText(Plugin.getDefault().getDriverVersion());
+		
+		textId.setText(Plugin.getDefault().getDriverBundleId());
+		textBundleVersion.setText(Plugin.getDefault().getDriverBundleVersion());
+		textBundleName.setText(Plugin.getDefault().getDriverBundleName());
+		textBundleVendor.setText(Plugin.getDefault().getDriverBundleVendor());
+		
+	}
 }
