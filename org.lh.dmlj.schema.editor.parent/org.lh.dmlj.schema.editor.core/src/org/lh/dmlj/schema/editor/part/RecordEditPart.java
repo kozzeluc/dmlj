kozzeluc/dmlj

commit 4114a8e486df447b5e7c1a6708140feebe706f48

Bug fix:  when changing an area name via the area dialog, not all record figures show the new area name.
This bug was introduced while creating the revised model change
notification system.

It is possible that, together with changing an area specification, the
area is being renamed as well; the record whose area specification is
changed will be refreshed already, but we need to make sure that the new
area name replaces the old one for all other records contained in that
area (we might unnecessarily do a refresh, but that's better than
missing an area rename).

org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/part/RecordEditPart.java
@@ -39,6 +39,7 @@ import org.lh.dmlj.schema.editor.anchor.LockedRecordSourceAnchor;
 import org.lh.dmlj.schema.editor.anchor.LockedRecordTargetAnchor;
 import org.lh.dmlj.schema.editor.anchor.ReconnectEndpointAnchor;
 import org.lh.dmlj.schema.editor.command.infrastructure.CommandExecutionMode;
+import org.lh.dmlj.schema.editor.command.infrastructure.IContextDataKeys;
 import org.lh.dmlj.schema.editor.command.infrastructure.IModelChangeProvider;
 import org.lh.dmlj.schema.editor.command.infrastructure.ModelChangeContext;
 import org.lh.dmlj.schema.editor.command.infrastructure.ModelChangeType;
@@ -112,13 +113,19 @@ public class RecordEditPart
 			
 			// the storage mode was changed
 			refreshVisuals();
-		} else if (context.getModelChangeType() == ModelChangeType.SET_PROPERTY) {
+		} else if (context.getModelChangeType() == ModelChangeType.SET_PROPERTY) {			
 			// the record name or containing area name has changed (execute/redo)
 			Boolean needToRefreshVisuals = (Boolean) context.getListenerData();
 			if (needToRefreshVisuals != null && needToRefreshVisuals.equals(Boolean.TRUE)) {
 				refreshVisuals();
 			}		
-		}		
+		} else if (context.getModelChangeType() == ModelChangeType.CHANGE_AREA_SPECIFICATION) {
+			// the containing area name was possibly renamed
+			Boolean needToRefreshVisuals = (Boolean) context.getListenerData();
+			if (needToRefreshVisuals != null && needToRefreshVisuals.equals(Boolean.TRUE)) {
+				refreshVisuals();
+			}
+		}
 	}
 	
 	@Override
@@ -140,6 +147,19 @@ public class RecordEditPart
 			// listener data, which we will pick up again when processing the after model change 
 			// event
 			context.setListenerData(Boolean.TRUE);
+		} else if (context.getModelChangeType() == ModelChangeType.CHANGE_AREA_SPECIFICATION &&
+				   !context.appliesTo(getModel())) {
+			
+			// it is possible that, together with changing an area specification, the area is 
+			// being renamed as well; the record whose area specification is changed will be
+			// refreshed already, but we need to make sure that the new area name replaces the old
+			// one (we might unnecessarily do a refresh, but that's better than missing an area
+			// rename)
+			String anotherRecordName = context.getContextData().get(IContextDataKeys.RECORD_NAME);
+			SchemaRecord anotherRecord = getModel().getSchema().getRecord(anotherRecordName);
+			if (anotherRecord.getAreaSpecification().getArea() == getModel().getAreaSpecification().getArea()) {
+				context.setListenerData(Boolean.TRUE);
+			}
 		}
 	}
 	

commit 56cd162665f3abd67e044dd738bd5460bb8a2003

Bug fix: renaming an area throws a NPE & destroys model change event processing
This bug was introduced with commit 4114a8e (Bug fix:  when changing an
area name via the area dialog, not all record figures show the new area
name.).  What we forgot while solving that problem, is that areas can
have a mix of records AND indexes; moreover, that commit didn't solve
the rename problem for indexes).

The fact that a NPE can prevent further model change notifications to be
dealt with normally has to taken care of by surrounding the invocation
of the before- and after model change methods on the listeners with a
try/catch block.

org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/part/RecordEditPart.java
@@ -32,8 +32,10 @@ import org.lh.dmlj.schema.ConnectionPart;
 import org.lh.dmlj.schema.LocationMode;
 import org.lh.dmlj.schema.MemberRole;
 import org.lh.dmlj.schema.OwnerRole;
+import org.lh.dmlj.schema.SchemaArea;
 import org.lh.dmlj.schema.SchemaPackage;
 import org.lh.dmlj.schema.SchemaRecord;
+import org.lh.dmlj.schema.Set;
 import org.lh.dmlj.schema.StorageMode;
 import org.lh.dmlj.schema.editor.anchor.LockedRecordSourceAnchor;
 import org.lh.dmlj.schema.editor.anchor.LockedRecordTargetAnchor;
@@ -151,14 +153,23 @@ public class RecordEditPart
 				   !context.appliesTo(getModel())) {
 			
 			// it is possible that, together with changing an area specification, the area is 
-			// being renamed as well; the record whose area specification is changed will be
-			// refreshed already, but we need to make sure that the new area name replaces the old
-			// one (we might unnecessarily do a refresh, but that's better than missing an area
-			// rename)
-			String anotherRecordName = context.getContextData().get(IContextDataKeys.RECORD_NAME);
-			SchemaRecord anotherRecord = getModel().getSchema().getRecord(anotherRecordName);
-			if (anotherRecord.getAreaSpecification().getArea() == getModel().getAreaSpecification().getArea()) {
-				context.setListenerData(Boolean.TRUE);
+			// being renamed as well; the record or system owned indexed set whose area 
+			// specification is changed will be refreshed already, but we need to make sure that the 
+			// new area name replaces the old one EVERYWHERE (we might unnecessarily do a refresh,  
+			// but that's better than missing an area rename)
+			SchemaArea modelArea = getModel().getAreaSpecification().getArea();
+			if (context.getContextData().containsKey(IContextDataKeys.RECORD_NAME)) {
+				String anotherRecordName = context.getContextData().get(IContextDataKeys.RECORD_NAME);
+				SchemaRecord anotherRecord = getModel().getSchema().getRecord(anotherRecordName);
+				if (anotherRecord.getAreaSpecification().getArea() == modelArea) {
+					context.setListenerData(Boolean.TRUE);
+				}
+			} else if (context.getContextData().containsKey(IContextDataKeys.SET_NAME)) {
+				String setName = context.getContextData().get(IContextDataKeys.SET_NAME);
+				Set set = getModel().getSchema().getSet(setName);
+				if (set.getSystemOwner().getAreaSpecification().getArea() == modelArea) {
+					context.setListenerData(Boolean.TRUE);
+				}
 			}
 		}
 	}

org.lh.dmlj.schema.editor.parent/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/part/SetDescriptionEditPart.java
@@ -31,11 +31,13 @@ import org.lh.dmlj.schema.ConnectionLabel;
 import org.lh.dmlj.schema.ConnectionPart;
 import org.lh.dmlj.schema.Connector;
 import org.lh.dmlj.schema.MemberRole;
+import org.lh.dmlj.schema.SchemaArea;
 import org.lh.dmlj.schema.SchemaPackage;
 import org.lh.dmlj.schema.SchemaRecord;
 import org.lh.dmlj.schema.Set;
 import org.lh.dmlj.schema.SystemOwner;
 import org.lh.dmlj.schema.editor.command.infrastructure.CommandExecutionMode;
+import org.lh.dmlj.schema.editor.command.infrastructure.IContextDataKeys;
 import org.lh.dmlj.schema.editor.command.infrastructure.IModelChangeProvider;
 import org.lh.dmlj.schema.editor.command.infrastructure.ModelChangeContext;
 import org.lh.dmlj.schema.editor.command.infrastructure.ModelChangeType;
@@ -123,6 +125,12 @@ public class SetDescriptionEditPart
 			if (needToRefreshVisuals != null && needToRefreshVisuals.equals(Boolean.TRUE)) {
 				refreshVisuals();
 			}		
+		} else if (context.getModelChangeType() == ModelChangeType.CHANGE_AREA_SPECIFICATION) {
+			// the containing area name was possibly renamed
+			Boolean needToRefreshVisuals = (Boolean) context.getListenerData();
+			if (needToRefreshVisuals != null && needToRefreshVisuals.equals(Boolean.TRUE)) {
+				refreshVisuals();
+			}
 		}
 	}
 	
@@ -147,6 +155,32 @@ public class SetDescriptionEditPart
 			// the listener data, which we will pick up again when processing the after model change 
 			// event
 			context.setListenerData(Boolean.TRUE);			
+		} else if (context.getModelChangeType() == ModelChangeType.CHANGE_AREA_SPECIFICATION &&
+				   getModel().getMemberRole().getSet().getSystemOwner() != null &&
+				   !context.appliesTo(getModel().getMemberRole().getSet())) {
+			
+			// it is possible that, together with changing an area specification, the area is 
+			// being renamed as well; the record or system owned indexed set whose area 
+			// specification is changed will be refreshed already, but we need to make sure that the 
+			// new area name replaces the old one EVERYWHERE (we might unnecessarily do a refresh,  
+			// but that's better than missing an area rename)
+			SchemaArea modelArea = 
+				getModel().getMemberRole().getSet().getSystemOwner().getAreaSpecification().getArea();
+			if (context.getContextData().containsKey(IContextDataKeys.RECORD_NAME)) {
+				String recordName = context.getContextData().get(IContextDataKeys.RECORD_NAME);
+				SchemaRecord record = 
+					getModel().getMemberRole().getSet().getSchema().getRecord(recordName);
+				if (record.getAreaSpecification().getArea() == modelArea) {
+					context.setListenerData(Boolean.TRUE);
+				}
+			} else if (context.getContextData().containsKey(IContextDataKeys.SET_NAME)) {
+				String anotherSetName = context.getContextData().get(IContextDataKeys.SET_NAME);
+				Set anotherSet = 
+					getModel().getMemberRole().getSet().getSchema().getSet(anotherSetName);
+				if (anotherSet.getSystemOwner().getAreaSpecification().getArea() == modelArea) {
+					context.setListenerData(Boolean.TRUE);
+				}
+			}
 		}
 	}
 	