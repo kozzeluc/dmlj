kozzeluc/dmlj

commit 3f19eb2c796db37718595f22361f136b8e40197f

Add Dark Theme support for the Record Info property section

Eclipse CA IDMS Schema Diagram Editor/bundles/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/property/section/RecordInfoSection.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018  Luc Hermans
+ * Copyright (C) 2019 Luc Hermans
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
@@ -17,19 +17,23 @@
 package org.lh.dmlj.schema.editor.property.section;
 
 import java.io.IOException;
+import java.util.Optional;
 
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+import org.eclipse.wb.swt.SWTResourceManager;
 import org.lh.dmlj.schema.SchemaRecord;
 import org.lh.dmlj.schema.editor.Plugin;
 import org.lh.dmlj.schema.editor.dictguide.DictguidesRegistry;
@@ -38,18 +42,26 @@ import org.lh.dmlj.schema.editor.property.RecordInfoValueObject;
 import org.lh.dmlj.schema.editor.template.RecordInfoTemplate;
 
 public class RecordInfoSection extends AbstractPropertiesSection {
-	
-	private static RecordInfoTemplate TEMPLATE = new RecordInfoTemplate();
+		
+	private static RecordInfoTemplate template = new RecordInfoTemplate();
 	private static final Logger logger = Logger.getLogger(Plugin.getDefault());
+		
+	private Control pageControl;
+	private Composite stackComposite;
+	private StackLayout stackLayout;
+	private Composite dataComposite;
+	private Browser browser;
+	private GridData gd_browser;
+	private Label emptyLabel;
 	
-	private Browser 		browser;
-	private Control 		control;
+	private Optional<String> html;
 	
 	private ControlListener controlListener = new ControlAdapter() {			
 		@Override
 		public void controlResized(ControlEvent e) {
-			Rectangle bounds = control.getBounds();
-			browser.setBounds(0, 0, 0, bounds.height);
+			stackComposite.setBounds(0, 0, pageControl.getBounds().width - 100, pageControl.getBounds().height);
+			gd_browser.widthHint = pageControl.getBounds().width - 100;
+			gd_browser.heightHint = pageControl.getBounds().height;
 		}			
 	};
 		
@@ -57,63 +69,72 @@ public class RecordInfoSection extends AbstractPropertiesSection {
 		super();		
 	}
 	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
-	public final void createControls(Composite parent, 
-							   		 TabbedPropertySheetPage aTabbedPropertySheetPage) {
-		
-		// We'll keep the browser width and height in sync with the tabbed
-		// property sheet page; it seems that when we set the width to zero that
-		// it always stretches horizontally, which is what we need; we need to
-		// take care of the heigth though.
-		
+	public final void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
 		super.createControls(parent, aTabbedPropertySheetPage);        		
+				
+		pageControl = aTabbedPropertySheetPage.getControl();
+		pageControl.addControlListener(controlListener);		
 		
-		control = aTabbedPropertySheetPage.getControl();
+		stackComposite = new Composite(parent, SWT.NONE);
+		stackComposite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		stackComposite.setBounds(0, 0, pageControl.getBounds().width - 100, pageControl.getBounds().height);
+		stackLayout = new StackLayout();
+		stackComposite.setLayout(stackLayout);
+				
+		dataComposite = new Composite(stackComposite, SWT.NONE);
+		dataComposite.setLayout(new GridLayout(2, false));
 		
-		browser = new Browser(parent, SWT.NONE);
-		Rectangle bounds = control.getBounds();
-		browser.setBounds(0, 0, 0, bounds.height);
+		browser = new Browser(dataComposite, SWT.FULL_SELECTION);
+		gd_browser = new GridData(SWT.LEFT, SWT.FILL, false, false, 2, 1);
+		gd_browser.verticalIndent = 5;
+		gd_browser.widthHint = pageControl.getBounds().width - 100;
+		gd_browser.heightHint = pageControl.getBounds().height;
+		browser.setLayoutData(gd_browser);
 		
-		// whenever the container control changes size, adjust the browser size
-		// as well...
-		control.addControlListener(controlListener);
-        
+		emptyLabel = new Label(stackComposite, SWT.NO_FOCUS);
+		stackLayout.topControl = emptyLabel;
+		stackComposite.layout();
 	}
 	
 	@Override
 	public void dispose() {
-		if (control != null && !control.isDisposed()) {
-			control.removeControlListener(controlListener);
+		if (pageControl != null && !pageControl.isDisposed()) {
+			pageControl.removeControlListener(controlListener);
 		}
 		super.dispose();
 	}
 	
 	@Override
 	public final void refresh() {
-		SchemaRecord record = (SchemaRecord) modelObject;
-		RecordInfoValueObject valueObject = null;
-		try {
-			valueObject = 
-				DictguidesRegistry.INSTANCE
-							  	  .getRecordInfoValueObject(record.getName());
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
+		if (html.isPresent()) {
+			browser.setText(html.get());
+			stackLayout.topControl = dataComposite;
+		} else {
+			stackLayout.topControl = emptyLabel;
 		}
-		if (valueObject == null) {
-			browser.setText("");
-			return;
-		}		
-		String html = TEMPLATE.generate(valueObject);		
-		browser.setText(html);		
+		stackComposite.layout();
 	}
 
 	@Override
 	public final void setInput(IWorkbenchPart part, ISelection selection) {
-		
-		super.setInput(part, selection);		
-		
-        Assert.isTrue(modelObject instanceof SchemaRecord, "not a SchemaRecord");
-                
+		super.setInput(part, selection);
+        try {
+        	String recordName = ((SchemaRecord) modelObject).getName();
+        	Optional<RecordInfoValueObject> data = 
+        		Optional.ofNullable(DictguidesRegistry.INSTANCE.getRecordInfoValueObject(recordName));
+        	if (data.isPresent()) {
+        		html = Optional.of(template.generate(data.get()));
+        	} else {
+        		html = Optional.empty();
+        	}
+        } catch (IOException e) {
+			logger.error(e.getMessage(), e);
+			html = Optional.empty();
+		}
 	}
 	
 }

Eclipse CA IDMS Schema Diagram Editor/bundles/org.lh.dmlj.schema.editor.core/META-INF/MANIFEST.MF
@@ -14,7 +14,8 @@ Require-Bundle: org.eclipse.ui,
  org.eclipse.ui.ide,
  org.eclipse.ui.views.properties.tabbed,
  org.lh.dmlj.schema.editor.groovy,
- org.lh.dmlj.schema.editor.model
+ org.lh.dmlj.schema.editor.model,
+ org.eclipse.e4.ui.css.swt.theme
 Bundle-ActivationPolicy: lazy
 Bundle-RequiredExecutionEnvironment: JavaSE-1.8
 Bundle-ClassPath: .

Eclipse CA IDMS Schema Diagram Editor/bundles/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/Plugin.java
@@ -164,6 +164,18 @@ public class Plugin extends AbstractUIPlugin implements IPropertyChangeListener,
 		// we may need to close .schemadsl editors on workbench shutdown 
 		PlatformUI.getWorkbench().addWorkbenchListener(workbenchListener);
 	}
+	
+	@SuppressWarnings("restriction")
+	public boolean isDarkThemeActive() {
+		// TODO find a better way to find out which theme is active and whether it is a dark theme
+		try {
+			org.eclipse.e4.ui.css.swt.theme.IThemeEngine engine = 
+				(org.eclipse.e4.ui.css.swt.theme.IThemeEngine) Display.getDefault().getData("org.eclipse.e4.ui.css.swt.theme");
+			return engine.getActiveTheme().getLabel().toLowerCase().contains("dark");
+		} catch (Throwable t) {
+			return false;
+		}
+	}
 
 	@Override
 	public boolean isDebugEnabled() {

Eclipse CA IDMS Schema Diagram Editor/bundles/org.lh.dmlj.schema.editor.core/templates/recordinfo.htmljet
@@ -1,6 +1,6 @@
 <%
 /**
- * Copyright (C) 2013  Luc Hermans
+ * Copyright (C) 2019  Luc Hermans
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
@@ -16,11 +16,33 @@
  * Contact information: kozzeluc@gmail.com.
  */
 %>
-<%@ jet package="org.lh.dmlj.schema.editor.template" imports="org.lh.dmlj.schema.editor.property.*" class="RecordInfoTemplate"%>
+<%@ jet package="org.lh.dmlj.schema.editor.template" imports="org.lh.dmlj.schema.editor.* org.lh.dmlj.schema.editor.property.*" class="RecordInfoTemplate"%>
 <%
 RecordInfoValueObject valueObject = (RecordInfoValueObject)argument;
 %>
 <HTML>
+<head>
+  <style>
+<% if (Plugin.getDefault().isDarkThemeActive()) { %>
+    body {
+      background-color: #262626;
+    }
+    h2 {
+      color: #c9c9c9;
+    }
+    p {
+      color: #c9c9c9;
+    }
+    table {
+      color: #c9c9c9;
+    }
+<% } else { %>
+    body {
+      background-color: #ececec;
+    }
+<% } %>
+  </style>
+</head>
 <TITLE><%= valueObject.getRecordName() %></TITLE>
 <BODY>
 <H2><%= valueObject.getRecordName() %></H2>

commit a96db3ee22685c41cca35b82a222dceeb4394108

Fix issue in Record Info property section (Windows)
This issue was introduced in commit 3f19eb2c.

Eclipse CA IDMS Schema Diagram Editor/bundles/org.lh.dmlj.schema.editor.core/src/org/lh/dmlj/schema/editor/property/section/RecordInfoSection.java
@@ -88,7 +88,7 @@ public class RecordInfoSection extends AbstractPropertiesSection {
 		dataComposite = new Composite(stackComposite, SWT.NONE);
 		dataComposite.setLayout(new GridLayout(2, false));
 		
-		browser = new Browser(dataComposite, SWT.FULL_SELECTION);
+		browser = new Browser(dataComposite, SWT.NONE);
 		gd_browser = new GridData(SWT.LEFT, SWT.FILL, false, false, 2, 1);
 		gd_browser.verticalIndent = 5;
 		gd_browser.widthHint = pageControl.getBounds().width - 100;